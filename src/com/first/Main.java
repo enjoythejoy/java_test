9e74ce2316cd964120dfa1bf4fc425d062556f89
Ashxumalourin <1732036346@qq.com>
2024-03-30 17:29:05 +0800

    test1

---
+++ src/com/first/Main.java
@@ -0,0 +1,17 @@
+package com.first;
+
+import java.util.*;
+import java.util.function.Consumer;
+
+public class Main {
+    public static void main(String[] args) {
+//        Set<Integer> s = new HashSet<>();
+//        HashSet<Character> c =  new LinkedHashSet<>();
+        TreeSet<String> ts = new TreeSet<>();
+        SortedMap<String, Integer> tm = new TreeMap<>();
+        ts.add("aba");
+        ts.add("ababa");
+        ts.forEach(System.out::println);
+    }
+
+}
